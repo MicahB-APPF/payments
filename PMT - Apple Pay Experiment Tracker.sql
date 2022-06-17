857d58588b25de2f7f5fccc7198cb3a115487ea0
MicahB-APPF <88748574+MicahB-APPF@users.noreply.github.com>
2022-06-17 11:21:03 -0700

    updated this to count all online transactions

--- PMT - Apple Pay Experiment Tracker.sql
+++ PMT - Apple Pay Experiment Tracker.sql
@@ -1,5 +1,5 @@
 ---PMT - Apple Pay Experiment Tracker (Cupola)
----Micah Brachman
+---Micah Brachman - code last updated 2022-06-17
 
 ---select payment methods
 with pmt as(
@@ -15,18 +15,18 @@ from tportal_users u
 join tportal_payments p on u.id = p.user_id and u.vhost = p.vhost
 left join tportal_payment_token_extensions pte on pte.vhost = p.vhost and pte.payment_token_id = p.payment_token_id
 where paid_on is not null
-and paid_on = '2022-06-13') --set to experiment start date
+and paid_on > '2022-06-15') -- experiment start date 6/16
 
 ---roll up stats for each experiment variation
 select variation_name,
-     count(distinct tte.vhost||tte.user_id) as users,
-     count(distinct case when payment_method = 'debit' then tte.vhost||tte.user_id end) as debit,
-     count(distinct case when payment_method = 'credit' then tte.vhost||tte.user_id end) as credit,
-     count(distinct case when payment_method = 'ach' then tte.vhost||tte.user_id end) as ach,
-     count(distinct case when is_apple_pay = TRUE then tte.vhost||tte.user_id end) as apple_pay,
-     count(distinct case when payment_method = 'debit' and is_apple_pay = TRUE then tte.vhost||tte.user_id end) as apple_pay_debit,
-     count(distinct case when payment_method = 'credit' and is_apple_pay = TRUE then tte.vhost||tte.user_id end) as apple_pay_credit
+     count(*) as online_payments,
+     count(case when payment_method = 'debit' then tte.vhost end) as debit,
+     count(case when payment_method = 'credit' then tte.vhost end) as credit,
+     count(case when payment_method = 'ach' then tte.vhost end) as ach,
+     count(case when is_apple_pay = TRUE then tte.vhost end) as apple_pay,
+     count(case when payment_method = 'debit' and is_apple_pay = TRUE then tte.vhost end) as apple_pay_debit,
+     count(case when payment_method = 'credit' and is_apple_pay = TRUE then tte.vhost end) as apple_pay_credit
 FROM tportal_tenant_full_stack_experiments tte
 JOIN pmt on pmt.vhost = tte.vhost and pmt.user_id = tte.user_id
-WHERE experiment_name = 'ri_modular_buyflow' ---change to 'tportal_apple_pay'
+WHERE experiment_name = 'tportal_apple_pay'
 group by variation_name
